45028b99f9addc36de7786700a7a2cb37da31366
mrizkyaly <rizkyaly20.ra@gmail.com>
2023-02-10 06:32:52 +0700

    create generate multiple language

--- kopywriter-site/components/kopywriter.tsx
+++ kopywriter-site/components/kopywriter.tsx
@@ -1,80 +1,118 @@
-import React from "react";
-import Form from "./form";
-import Hasil from "./hasil";
-import Image from "next/image";
-import logo from "../public/logoipsum.svg";
+import React from 'react';
+import Form from './form';
+import Hasil from './hasil';
+import Image from 'next/image';
+import logo from '../public/logoipsum.svg';
 
 const Kopywriter: React.FC = () => {
-  const CHARACTER_LIMIT: number = 32;
-  const ENDPOINT: string = `https://k7hs8a7bp7.execute-api.us-east-1.amazonaws.com/prod/generate_copywrite_dan_keywords`;
-  const [prompt, setPrompt] = React.useState("");
-  const [sentence, setSentence] = React.useState("");
-  const [keywords, setKeywords] = React.useState([]);
-  const [hasHasil, setHasHasil] = React.useState(false);
-  const [isLoading, setIsLoading] = React.useState(false);
+    const CHARACTER_LIMIT: number = 32;
+    const ENDPOINT_IND: string =
+        'https://xmbqbxjvyc.execute-api.ap-southeast-1.amazonaws.com/prod/generate_copywrite_dan_keywords';
+    const ENDPOINT_ENG: string = `https://k7hs8a7bp7.execute-api.us-east-1.amazonaws.com/prod/generate_copywrite_dan_keywords`;
+    const [prompt, setPrompt] = React.useState('');
+    const [sentence, setSentence] = React.useState('');
+    const [keywords, setKeywords] = React.useState([]);
+    const [hasHasil, setHasHasil] = React.useState(false);
+    const [isLoading, setIsLoading] = React.useState(false);
+    const [language, setLanguage] = React.useState('');
 
-  const onSubmit = () => {
-    console.log("Submitting:" + prompt);
-    setIsLoading(true);
-    fetch(`${ENDPOINT}?prompt=${prompt}`)
-      .then((res) => res.json())
-      .then(onHasil);
-  };
+    console.log('Language:', language);
 
-  const onHasil = (data: any) => {
-    setSentence(data.sentence);
-    setKeywords(data.keywords);
-    setHasHasil(true);
-    setIsLoading(false);
-  };
+    const onSubmit = () => {
+        console.log('Submitting:' + prompt);
+        setIsLoading(true);
 
-  const onReset = () => {
-    setPrompt("");
-    setHasHasil(false);
-    setIsLoading(false);
-  };
+        if (language === 'ENG') {
+            fetch(`${ENDPOINT_ENG}?prompt=${prompt}`)
+                .then((res) => res.json())
+                .then(onHasil);
+        } else if (language == 'IND') {
+            fetch(`${ENDPOINT_IND}?prompt=${prompt}`)
+                .then((res) => res.json())
+                .then(onHasil);
+        }
+    };
 
-  let elemenTerlihat = null;
+    const onHasil = (data: any) => {
+        setSentence(data.sentence);
+        setKeywords(data.keywords);
+        setHasHasil(true);
+        setIsLoading(false);
+    };
 
-  if (hasHasil) {
-    elemenTerlihat = (
-      <Hasil
-        sentence={sentence}
-        keywords={keywords}
-        onBack={onReset}
-        prompt={prompt}
-      />
-    );
-  } else {
-    elemenTerlihat = (
-      <Form
-        prompt={prompt}
-        setPrompt={setPrompt}
-        onSubmit={onSubmit}
-        isLoading={isLoading}
-        characterLimit={CHARACTER_LIMIT}
-      />
-    );
-  }
+    const onReset = () => {
+        setPrompt('');
+        setHasHasil(false);
+        setIsLoading(false);
+    };
 
-  return (
-    <>
-      <div className="h-screen flex">
-        <div className="max-w-md m-auto p-2">
-          <div className="bg-slate-600 p-5 rounded-md text-white">
-            <div className="text-center my-5">
-              <Image src={logo} width={64} height={64} />
-              <h1 className=" text-3xl text-white font-light mt-4">
-                Kopywriter
-              </h1>
-              <div>AI Generated Copywriting and Keywords</div>
+    let elemenTerlihat = null;
+
+    if (hasHasil) {
+        elemenTerlihat = (
+            <Hasil
+                sentence={sentence}
+                keywords={keywords}
+                onBack={onReset}
+                prompt={prompt}
+            />
+        );
+    } else {
+        elemenTerlihat = (
+            <Form
+                prompt={prompt}
+                setPrompt={setPrompt}
+                onSubmit={onSubmit}
+                isLoading={isLoading}
+                characterLimit={CHARACTER_LIMIT}
+            />
+        );
+    }
+
+    return (
+        <>
+            <div className='h-screen flex'>
+                <div className='max-w-md m-auto p-2'>
+                    <div className='bg-slate-600 p-5 rounded-md text-white'>
+                        <div className='flex row-auto items-center justify-end'>
+                            <div className='col-auto mr-2'>
+                                <button
+                                    className='bg-gradient-to-r from-teal-400 
+to-blue-500 disabled:opacity-50 w-full py-2 px-4 rounded-md text-lg'
+                                    value={'IND'}
+                                    onClick={(e) =>
+                                        setLanguage(e.currentTarget.value)
+                                    }
+                                >
+                                    IND
+                                </button>
+                            </div>
+                            <div className='col-auto'>
+                                <button
+                                    className='bg-gradient-to-r from-teal-400 
+to-blue-500 disabled:opacity-50 w-full py-2 px-4 rounded-md text-lg'
+                                    value={'ENG'}
+                                    onClick={(e) =>
+                                        setLanguage(e.currentTarget.value)
+                                    }
+                                >
+                                    ENG
+                                </button>
+                            </div>
+                        </div>
+                        <div className='text-center my-5'>
+                            <Image alt='' src={logo} width={64} height={64} />
+                            <h1 className=' text-3xl text-white font-light mt-4'>
+                                Kopywriter
+                            </h1>
+                            <div>AI Generated Copywriting and Keywords</div>
+                        </div>
+                        {elemenTerlihat}
+                    </div>
+                </div>
             </div>
-            {elemenTerlihat}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Kopywriter;
